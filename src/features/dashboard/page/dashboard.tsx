0a5119e1eee02f1b1252d8252e5284a0954f84df
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2025-02-26 22:29:31 -0600

    confir

--- src/features/dashboard/page/dashboard.tsx
+++ src/features/dashboard/page/dashboard.tsx
@@ -2,7 +2,7 @@ import { InfoUserType } from "@/stores/auth.store";
 import {  useRouteLoaderData } from "react-router-dom";
 
 function Dashboard() {
-  const userData = useRouteLoaderData("root") as InfoUserType;
+  // const userData = useRouteLoaderData("root") as InfoUserType;
 
 
   return (
